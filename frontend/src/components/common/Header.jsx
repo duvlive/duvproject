ed99f0ec30203afa6b2136712c2e2f509e921927
Haruna Popoola <haruna.popoola@andela.com>
2019-06-23 11:20:56 +0100

    add top menu

--- frontend/src/components/common/Header.jsx
+++ frontend/src/components/common/Header.jsx
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useState, } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
+import { Link, } from '@reach/router';
 import IconPhone from 'assets/icons/phone.svg';
+import WhiteLogo from 'assets/logo/white-white.svg';
+
+const menus = [
+  { name: 'Home', to: '/', },
+  { name: 'How it Works', to: '/how-it-works', },
+  { name: 'Hire Entertainers', to: '/hire-entertainers', },
+  { name: 'Upcoming Events', to: '/upcoming-events', },
+  { name: 'BrainBox', to: '/brain-box', },
+];
 
 const Header = () => {
+  const [isOpen, setOpen,] = useState(false);
   return (
-    <div className="top-header">
+    <header>
+      <Header.TopNav />
+      <div className="container-fluid">
+        <Navbar color="transparent" dark expand="md">
+          <NavbarBrand tag={Link} to="/">
+            <img alt="Duv Live White Logo" height="75" src={WhiteLogo} />
+          </NavbarBrand>
+          <NavbarToggler onClick={() => setOpen(!isOpen)} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <Header.Menus menus={menus} />
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    </header>
+  );
+};
+
+Header.TopNav = () => {
+  return (
+    <section className="top-header">
       <div className="container-fluid">
         <div className="row">
           <div className="top-header__left col-sm-6">
@@ -31,8 +72,22 @@ const Header = () => {
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
+};
+
+Header.Menus = ({ menus, }) => {
+  return menus.map(menu => (
+    <NavItem>
+      <NavLink getProps={isActive} tag={Link} to={menu.to}>
+        {menu.name}
+      </NavLink>
+    </NavItem>
+  ));
+};
+
+const isActive = ({ isCurrent, }) => {
+  return isCurrent ? { className: 'active nav-link', } : null;
 };
 
 export default Header;
