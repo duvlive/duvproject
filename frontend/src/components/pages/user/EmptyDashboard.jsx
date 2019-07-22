8cbae5d39aa5d2659427a1fc9abd65ca0dde5dd6
Haruna Popoola <haruna.popoola@andela.com>
2019-07-23 00:17:19 +0100

    Add Empty Dashboard

--- frontend/src/components/pages/user/EmptyDashboard.jsx
+++ frontend/src/components/pages/user/EmptyDashboard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import TopMessage from 'components/common/TopMessage';
 import NoContent from 'components/common/NoContent';
 import Image from 'components/common/Image';
-import { randomItem, getItems } from 'utils/helpers';
+import { randomItem } from 'utils/helpers';
 import djLists from 'data/entertainers/djs';
 import mcLists from 'data/entertainers/mcs';
 import lbLists from 'data/entertainers/live-bands';
@@ -26,15 +26,16 @@ const EmptyDashboard = () => {
                 />
               </div>
             </div>
-            <EmptyDashboard.AuctionTable
-              entertainerList={getItems(djLists, 3)}
-            />
+            <EmptyDashboard.AuctionTable />
           </div>
           <div className="col-sm-4">
             <EmptyDashboard.RecommendedTable
-              entertainerList={[randomItem(lbLists), randomItem(mcLists)]}
+              entertainerList={[
+                randomItem([...lbLists, ...djLists]),
+                randomItem(mcLists)
+              ]}
             />
-            <EmptyDashboard.PendingReview entertainer={djLists[7]} />
+            <EmptyDashboard.PendingReview />
           </div>
         </div>
       </section>
@@ -42,81 +43,26 @@ const EmptyDashboard = () => {
   );
 };
 
-EmptyDashboard.PendingReview = ({ entertainer }) => (
+EmptyDashboard.PendingReview = () => (
   <div className="card card-custom">
     <div className="card-body">
+      <h5 className="card-title red header__with-border">Pending Review</h5>
+      <NoContent text="No pending Reviews." />
+    </div>
+  </div>
+);
+
+EmptyDashboard.AuctionTable = () => (
+  <div className="card card-custom">
+    <div className="card-body">
+      <h5 className="card-title blue">Auction (Recent Bids)</h5>
       <NoContent
-        isButton
-        linkText="You have no current Auctions."
+        linkText="Learn how it works"
         linkTo="/user/events/new"
-        text="Learn how it works"
+        text="You have no current Auctions."
       />
     </div>
   </div>
-);
-
-EmptyDashboard.AuctionTable = ({ entertainerList }) => (
-  <div className="card card-custom">
-    <div className="card-body">
-      <h5 className="card-title blue">
-        Auction (Recent Bids) <br />
-        <small className="small--2 text-gray">
-          Celebration Party for Wifey on{' '}
-          <span className="text-gray">Apr. 7, 2019</span>
-        </small>
-      </h5>
-      <div className="card-subtitle--3 text-gray mb-3">
-        Party DJ
-        <span className="float-right small--2 text-gray text-normal">
-          Closes on 17th Apr, 2019
-        </span>
-      </div>
-
-      <div className="table-responsive">
-        <table className="table table-dark">
-          <tbody>
-            {entertainerList.map(entertainer => (
-              <EmptyDashboard.AuctionRow
-                entertainer={entertainer}
-                key={entertainer.stage_name + entertainer.id}
-              />
-            ))}
-          </tbody>
-          <tfoot>
-            <tr>
-              <td colSpan="6">
-                <h5 className="main-app__subtitle mt-2 mb-0">
-                  Your Budget: N80,000
-                </h5>
-              </td>
-            </tr>
-          </tfoot>
-        </table>
-      </div>
-    </div>
-  </div>
-);
-
-EmptyDashboard.AuctionRow = ({ entertainer }) => (
-  <tr>
-    <th scope="row">
-      <Image
-        className="avatar--small"
-        name={entertainer.stage_name}
-        src={entertainer.img.profile}
-      />
-    </th>
-    <td>{entertainer.stage_name}</td>
-    <td>{entertainer.average_ratings}</td>
-    <td className="text-red">N70,000</td>
-    <td className="text-right">
-      <span className="text-yellow">View Profile</span>
-    </td>
-    <td className="text-right">
-      {' '}
-      <span className="text-green">Approve</span>
-    </td>
-  </tr>
 );
 
 EmptyDashboard.RecommendedTable = ({ entertainerList }) => (
