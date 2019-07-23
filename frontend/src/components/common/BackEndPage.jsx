5d5e54a61354764e49c88dd7a097f64979249c86
Haruna Popoola <haruna.popoola@andela.com>
2019-07-23 11:02:19 +0100

    Create generic landing section component

--- frontend/src/components/common/BackEndPage.jsx
+++ frontend/src/components/common/BackEndPage.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Sidebar from 'components/common/Sidebar';
 import LandingSection from 'components/common/LandingSection';
 
-const BackEndPage = ({ children }) => {
+const BackEndPage = ({ children, title, subtitle }) => {
+  const [showSidebar, setShowSidebar] = useState(false);
   return (
     <div>
-      <Sidebar />
+      <Sidebar close={() => setShowSidebar(false)} show={showSidebar} />
       <div className="content-page">
-        <LandingSection />
+        <LandingSection
+          isDashboard
+          showSidebar={() => setShowSidebar(true)}
+          subtitle={subtitle}
+          title={title}
+        />
         {children}
       </div>
     </div>
@@ -16,7 +22,13 @@ const BackEndPage = ({ children }) => {
 };
 
 BackEndPage.propTypes = {
-  children: PropTypes.node.isRequired
+  children: PropTypes.node.isRequired,
+  subtitle: PropTypes.string,
+  title: PropTypes.string.isRequired
+};
+
+BackEndPage.defaultProps = {
+  subtitle: null
 };
 
 export default BackEndPage;
--- frontend/src/components/common/LandingSection.jsx
+++ frontend/src/components/common/LandingSection.jsx
@@ -1,17 +1,46 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import Header from 'components/common/Header';
+import Humanize from 'humanize-plus';
 import TopBar from 'components/common/TopBar';
 
-const LandingSection = () => (
+const LandingSection = ({ showSidebar, isDashboard, title, subtitle }) => (
   <section className="landing">
-    <div className="card card__dashboard bg-dark text-white">
-      <TopBar />
+    <div
+      className={classNames(
+        'card bg-dark text-white',
+        {
+          card__dashboard: isDashboard
+        },
+        {
+          card__menu: !isDashboard
+        }
+      )}
+    >
+      {isDashboard ? <TopBar showSidebar={showSidebar} /> : <Header />}
       <div className="card-img-overlay">
         <div className="card-img-overlay__content">
-          <h2 className="card-title">Dashboard</h2>
-          <p className="card-subtitle">DUV LIVE &nbsp;/ &nbsp;Dashboard</p>
+          <h2 className="card-title">{title && Humanize.capitalize(title)}</h2>
+          <p className="card-subtitle">
+            DUV LIVE &nbsp;/ &nbsp;{subtitle || title}
+          </p>
         </div>
       </div>
     </div>
   </section>
 );
+
+LandingSection.propTypes = {
+  isDashboard: PropTypes.bool,
+  showSidebar: PropTypes.bool,
+  subtitle: PropTypes.string,
+  title: PropTypes.string.isRequired
+};
+
+LandingSection.defaultProps = {
+  isDashboard: false,
+  showSidebar: false,
+  subtitle: null
+};
 export default LandingSection;
