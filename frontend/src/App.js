03572edfa5f40c9ba28899c5e37ca897436e7792
Haruna Popoola <haruna.popoola@andela.com>
2019-09-29 22:01:05 +0100

    Split routes into multiple components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,10 @@
 import React from 'react';
 import { Router } from '@reach/router';
 import { Helmet } from 'react-helmet';
-import Home from 'components/pages/frontend/Home';
-import HowItWorks from 'components/pages/frontend/HowItWorks';
 import 'assets/sass/App.scss';
-import UpcomingEvents from 'components/pages/frontend/UpcomingEvents';
-import HireEntertainers from 'components/pages/frontend/HireEntertainers';
-import Help from 'components/pages/frontend/Help';
-import TermsOfUse from 'components/pages/frontend/TermsOfUse';
-import PrivacyPolicy from 'components/pages/frontend/PrivacyPolicy';
-import Login from 'components/pages/auth/Login';
-import Logout from 'components/pages/auth/Logout';
-import Register from 'components/pages/auth/Register';
-import SingleEntertainer from 'components/pages/frontend/SingleEntertainer';
-import SingleEvent from 'components/pages/frontend/SingleEvent';
 import { ScrollToTop } from 'components/custom/ScrollToTop';
-import ForgotPassword from 'components/pages/auth/ForgotPassword';
-import UserPage from 'components/pages/user/Index';
-import Dashboard from 'components/pages/user/Dashboard';
-import EmptyDashboard from 'components/pages/user/EmptyDashboard';
-import Auctions from 'components/pages/user/Auctions';
-import Events from 'components/pages/user/Events';
-import Notifications from 'components/pages/user/Notifications';
-import UserHelp from 'components/pages/user/Help';
-import Payments from 'components/pages/user/Payments';
-import SelectEntertainer from 'components/pages/user/HireEntertainers';
-import NewEvent from 'components/pages/user/NewEvent';
-import ViewEvent from 'components/pages/user/ViewEvent';
-import Bids from 'components/pages/user/Bids';
-import EditProfile from 'components/pages/user/EditProfile';
-import ChangePassword from 'components/pages/user/ChangePassword';
-import RegisterAsEntertainer from 'components/pages/user/RegisterAsEntertainer';
-import Approval from 'components/pages/user/Approval';
+import FrontPageRouter from 'routers/FrontPageRouter';
+import UserRouter from 'routers/UserRouter';
 import FormikForm from 'components/forms/FormikForm';
 
 function App() {
@@ -42,42 +15,10 @@ function App() {
       </Helmet>
       <Router primary={false}>
         <ScrollToTop path="/">
-          <Home path="/" />
-          <HowItWorks path="how-it-works" />
-          <UpcomingEvents path="upcoming-events" />
-          <HireEntertainers path="hire-entertainers" />
-          <Help path="help" />
-          <TermsOfUse path="terms-of-use" />
-          <PrivacyPolicy path="privacy-policy" />
-          <Login path="login" />
-          <Logout path="logout" />
-          <ForgotPassword path="forgot-password" />
-          <Register path="register" />
-          <Register path="register/:type" />
-          <SingleEntertainer path="entertainer/:slug" />
-          <SingleEvent path="event/:slug" />
+          <FrontPageRouter path="/*" />
+          <UserRouter path="user/*" />
 
-          <UserPage path="/user">
-            <Dashboard path="/dashboard" />
-            <Auctions path="/auctions" />
-            <Events path="/events" />
-            <Notifications path="/notifications" />
-            <UserHelp path="/help" />
-            <Payments path="/payments-history" />
-            <SelectEntertainer path="/hire-entertainer" />
-            <SelectEntertainer path="/hire-entertainer/:event_id" />
-            <NewEvent path="events/new" />
-            <NewEvent path="events/new/:hire_type" />
-            <ViewEvent path="events/:id" />
-            {/* <AddEntertainerToEvent path="events/:id/add-entertainer/:type" /> */}
-            <Bids path="/auction/bids" />
-            <Approval path="/approval" />
-            <EditProfile path="/edit-profile" />
-            <ChangePassword path="/change-password" />
-            <RegisterAsEntertainer path="/register-as-entertainer" />
-            <EmptyDashboard default />
-          </UserPage>
-
+          {/* To remove */}
           <FormikForm path="formik" />
         </ScrollToTop>
       </Router>
