2d45db365cc81aa9793dd8ee8d01613358cde1d6
Haruna Popoola <haruna.popoola@andela.com>
2019-11-03 16:43:32 +0100

    Update administrator content

--- frontend/src/components/pages/administrator/RegisteredUsers.jsx
+++ frontend/src/components/pages/administrator/RegisteredUsers.jsx
@@ -15,41 +15,46 @@ const RegisteredUsers = () => (
               <tr>
                 <th>S/N</th>
                 <th>Name</th>
-                <th>Stage Name</th>
-                <th>Type</th>
-                <th>Location</th>
-                <th>Verified</th>
                 <th></th>
               </tr>
               <tr>
                 <td>01.</td>
                 <td>Olawale Adebisi</td>
-                <td>DJ Proton</td>
-                <td>DJ</td>
-                <td>Lagos State</td>
-                <td>NO</td>
                 <td>
                   <Link to="#">Manage</Link>
                 </td>
               </tr>
               <tr>
                 <td>02.</td>
-                <td>Precious Jewel</td>
-                <td>Holy Guys</td>
-                <td>Live Band</td>
-                <td>Lagos State</td>
-                <td>NO</td>
+                <td>Adewale Ayuba</td>
                 <td>
                   <Link to="#">Manage</Link>
                 </td>
               </tr>
               <tr>
                 <td>03.</td>
+                <td>Nnamdi Emeka</td>
+                <td>
+                  <Link to="#">Manage</Link>
+                </td>
+              </tr>
+              <tr>
+                <td>04.</td>
                 <td>Olawale Adebisi</td>
-                <td>Sweet Mouth</td>
-                <td>MC</td>
-                <td>Port Harcourt</td>
-                <td>YES</td>
+                <td>
+                  <Link to="#">Manage</Link>
+                </td>
+              </tr>
+              <tr>
+                <td>05.</td>
+                <td>Adewale Ayuba</td>
+                <td>
+                  <Link to="#">Manage</Link>
+                </td>
+              </tr>
+              <tr>
+                <td>06.</td>
+                <td>Nnamdi Emeka</td>
                 <td>
                   <Link to="#">Manage</Link>
                 </td>
