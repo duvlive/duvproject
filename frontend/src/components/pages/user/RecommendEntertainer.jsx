5100c1b0922cc9ee1837d503e205ecf865682c18
Haruna Popoola <haruna.popoola@andela.com>
2019-12-21 23:58:40 +0100

    add recommend entertainer

---
+++ frontend/src/components/pages/user/RecommendEntertainer.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import TopMessage from 'components/common/layout/TopMessage';
+import BackEndPage from 'components/common/layout/BackEndPage';
+import FilterEntertainerFields from 'components/common/entertainers/FilterEntertainerFields';
+import EntertainersSearchResult from 'components/common/entertainers/EntertainersSearchResult';
+import { Row } from 'reactstrap';
+
+const SearchEntertainer = ({ event_type }) => {
+  return (
+    <BackEndPage title="Search Entertainer">
+      <div className="main-app">
+        <TopMessage message="Search Entertainer" />
+
+        <section className="app-content">
+          <Row>
+            <FilterEntertainerFields />
+            <EntertainersSearchResult />
+          </Row>
+        </section>
+      </div>
+    </BackEndPage>
+  );
+};
+
+SearchEntertainer.propTypes = {
+  event_type: PropTypes.string
+};
+
+SearchEntertainer.defaultProps = {
+  event_type: ''
+};
+
+export default SearchEntertainer;
