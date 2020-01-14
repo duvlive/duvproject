1672b7124c29bc58bf7534b9ecfe3b3e95bb24d2
Seyi Keye <aromokeyes2@gmail.com>
2020-01-14 18:34:26 +0100

    convert tab to space

--- src-server/server.js
+++ src-server/server.js
@@ -16,8 +16,8 @@ const port = parseInt(process.env.PORT, 10) || 8080;
 const app = express();
 
 app.use((req, res, next) => {
-	global.host = req.protocol + '://' + req.get('host');
-	next();
+  global.host = req.protocol + '://' + req.get('host');
+  next();
 });
 
 app.use(passport.initialize());
@@ -26,41 +26,41 @@ const googleStrategy = GoogleStrategy.OAuth2Strategy;
 
 passport.authenticate('facebook');
 passport.use(
-	new facebookStrategy(
-		{
-			clientID: process.env.FACEBOOK_APP_ID,
-			clientSecret: process.env.FACEBOOK_APP_SECRET,
-			callbackURL: `${global.host}/api/v1/auth/facebook/callback`,
-			profileFields: ['emails', 'name']
-		},
-		function(accessToken, refreshToken, profile, done) {
-			const {
-				last_name: lastName,
-				first_name: firstName,
-				email
-			} = profile._json;
-			done(null, { firstName, lastName, email });
-		}
-	)
+  new facebookStrategy(
+    {
+      clientID: process.env.FACEBOOK_APP_ID,
+      clientSecret: process.env.FACEBOOK_APP_SECRET,
+      callbackURL: `${global.host}/api/v1/auth/facebook/callback`,
+      profileFields: ['emails', 'name']
+    },
+    function(accessToken, refreshToken, profile, done) {
+      const {
+        last_name: lastName,
+        first_name: firstName,
+        email
+      } = profile._json;
+      done(null, { firstName, lastName, email });
+    }
+  )
 );
 
 passport.use(
-	new googleStrategy(
-		{
-			clientID: process.env.GOOGLE_CLIENT_ID,
-			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-			callbackURL: `${global.host}/api/v1/auth/google/callback`,
-			profileFields: ['emails', 'name']
-		},
-		function(accessToken, refreshToken, profile, done) {
-			const {
-				email,
-				family_name: lastName,
-				given_name: firstName
-			} = profile._json;
-			done(null, { firstName, lastName, email });
-		}
-	)
+  new googleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: `${global.host}/api/v1/auth/google/callback`,
+      profileFields: ['emails', 'name']
+    },
+    function(accessToken, refreshToken, profile, done) {
+      const {
+        email,
+        family_name: lastName,
+        given_name: firstName
+      } = profile._json;
+      done(null, { firstName, lastName, email });
+    }
+  )
 );
 
 app.use(logger('dev'));
@@ -74,12 +74,12 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'build')));
 
 if (process.env.NODE_ENV === 'production') {
-	// Handle React routing, return all requests to React app
-	app.get('*', function(req, res) {
-		res.sendFile(path.join(__dirname, 'build', 'index.html'));
-	});
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
 }
 
 app.listen(port, () => {
-	console.info(`Started up the server at port ${port}`);
+  console.info(`Started up the server at port ${port}`);
 });
