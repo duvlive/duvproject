1db72d488ca83d850bfa68e7aa55eebe08ef55bb
Seyi Keye <aromokeyes2@gmail.com>
2020-01-14 19:37:21 +0100

    fix more linting issues

--- src-server/server/routes.js
+++ src-server/server/routes.js
@@ -1,108 +1,108 @@
-import { Router } from "express";
+import { Router } from 'express';
 import {
-  UserController,
-  UserProfileController,
-  BankDetailController,
-  ContactController
-} from "./controllers";
-import Authentication from "./middleware/authentication";
-import passport from "passport";
-import { generateEmailTemplate } from "./MailSender";
+	UserController,
+	UserProfileController,
+	BankDetailController,
+	ContactController
+} from './controllers';
+import Authentication from './middleware/authentication';
+import passport from 'passport';
+import { generateEmailTemplate } from './MailSender';
 
 const router = Router();
 
 // welcome route
-router.get("/api/v1", (_, res) =>
-  res
-    .status(200)
-    .json({ success: "Welcome to DUV Live API V1. Live your Best Live" })
+router.get('/api/v1', (_, res) =>
+	res
+		.status(200)
+		.json({ success: 'Welcome to DUV Live API V1. Live your Best Live' })
 );
 
 // TODO: create controller for this
 // email route
-router.get("/email-template", (req, res) => {
-  const options = {
-    firstName: "[FirstName]",
-    contentTop: "[Top Content]",
-    contentBottom: "[Bottom Content]",
-    ButtonText: "[Button Text]",
-    ButtonLink: "https://www.duvlive.herokuapp.com"
-  };
-  generateEmailTemplate(options).then(data => {
-    return res.send(data);
-  });
+router.get('/email-template', (req, res) => {
+	const options = {
+		firstName: '[FirstName]',
+		contentTop: '[Top Content]',
+		contentBottom: '[Bottom Content]',
+		ButtonText: '[Button Text]',
+		ButtonLink: 'https://www.duvlive.herokuapp.com'
+	};
+	generateEmailTemplate(options).then(data => {
+		return res.send(data);
+	});
 });
 
 // user routes.
-router.post("/api/v1/users", UserController.createUser);
-router.post("/api/v1/users/login", UserController.userLogin);
-router.get("/api/v1/users/logout", UserController.userLogout);
-router.get("/api/v1/users/activate", UserController.activateUser);
-router.post("/api/v1/users/password-reset", UserController.passwordReset);
-router.post("/api/v1/users/update-password", UserController.updatePassword);
+router.post('/api/v1/users', UserController.createUser);
+router.post('/api/v1/users/login', UserController.userLogin);
+router.get('/api/v1/users/logout', UserController.userLogout);
+router.get('/api/v1/users/activate', UserController.activateUser);
+router.post('/api/v1/users/password-reset', UserController.passwordReset);
+router.post('/api/v1/users/update-password', UserController.updatePassword);
 
 router.get(
-  "/api/v1/auth/facebook",
-  passport.authenticate("facebook", { scope: ["email"] })
+	'/api/v1/auth/facebook',
+	passport.authenticate('facebook', { scope: ['email'] })
 );
 router.get(
-  "/api/v1/auth/facebook/callback",
-  passport.authenticate("facebook", { session: false }),
-  UserController.socialLogin
+	'/api/v1/auth/facebook/callback',
+	passport.authenticate('facebook', { session: false }),
+	UserController.socialLogin
 );
 
 router.get(
-  "/api/v1/auth/google",
-  passport.authenticate("google", {
-    scope: [
-      "https://www.googleapis.com/auth/plus.login",
-      "https://www.googleapis.com/auth/userinfo.profile",
-      "https://www.googleapis.com/auth/userinfo.email"
-    ]
-  })
+	'/api/v1/auth/google',
+	passport.authenticate('google', {
+		scope: [
+			'https://www.googleapis.com/auth/plus.login',
+			'https://www.googleapis.com/auth/userinfo.profile',
+			'https://www.googleapis.com/auth/userinfo.email'
+		]
+	})
 );
 router.get(
-  "/api/v1/auth/google/callback",
-  passport.authenticate("google", { session: false }),
-  UserController.socialLogin
+	'/api/v1/auth/google/callback',
+	passport.authenticate('google', { session: false }),
+	UserController.socialLogin
 );
 
 router
-  .route("/api/v1/users/editUser")
-  .put(
-    Authentication.verifyToken,
-    Authentication.validateUser,
-    UserController.editUser
-  );
+	.route('/api/v1/users/editUser')
+	.put(
+		Authentication.verifyToken,
+		Authentication.validateUser,
+		UserController.editUser
+	);
 router
-  .route("/api/v1/users/editEntertainer")
-  .put(
-    Authentication.verifyToken,
-    Authentication.validateEntertainer,
-    UserController.editEntertainer
-  );
+	.route('/api/v1/users/editEntertainer')
+	.put(
+		Authentication.verifyToken,
+		Authentication.validateEntertainer,
+		UserController.editEntertainer
+	);
 router
-  .route("/api/v1/users/updateUserProfile")
-  .put(
-    Authentication.verifyToken,
-    Authentication.validateEntertainer,
-    UserProfileController.updateUserAndUserProfile
-  );
+	.route('/api/v1/users/updateUserProfile')
+	.put(
+		Authentication.verifyToken,
+		Authentication.validateEntertainer,
+		UserProfileController.updateUserAndUserProfile
+	);
 
 // bankDetails routes
 router
-  .route("/api/v1/bankDetail")
-  .all(Authentication.verifyToken, Authentication.validateEntertainer)
-  .post(BankDetailController.updateUserBankDetail)
-  .put(BankDetailController.updateUserBankDetail)
-  .get(BankDetailController.getUserBankDetail);
+	.route('/api/v1/bankDetail')
+	.all(Authentication.verifyToken, Authentication.validateEntertainer)
+	.post(BankDetailController.updateUserBankDetail)
+	.put(BankDetailController.updateUserBankDetail)
+	.get(BankDetailController.getUserBankDetail);
 
 // Contact routes
 router
-  .route("/api/v1/contact")
-  .all(Authentication.verifyToken, Authentication.validateEntertainer)
-  .post(ContactController.updateUserContact)
-  .put(ContactController.updateUserContact)
-  .get(ContactController.getUserContact);
+	.route('/api/v1/contact')
+	.all(Authentication.verifyToken, Authentication.validateEntertainer)
+	.post(ContactController.updateUserContact)
+	.put(ContactController.updateUserContact)
+	.get(ContactController.getUserContact);
 
 export default router;
