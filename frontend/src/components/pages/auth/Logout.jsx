5544a00b6acdb793bdb12bc11ce59ee63cbbec85
Haruna Popoola <haruna.popoola@andela.com>
2020-01-19 15:05:32 +0100

    add localStorage function

--- frontend/src/components/pages/auth/Logout.jsx
+++ frontend/src/components/pages/auth/Logout.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { clearStorage } from 'utils/localStorage';
 import { Redirect } from '@reach/router';
 
 const Logout = () => {
+  clearStorage();
   return <Redirect to="login" />;
 };
 
