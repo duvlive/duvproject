0d488623a656479644977e16b38cc7efdce636da
Haruna Popoola <haruna.popoola@yahoo.com>
2020-03-21 09:01:53 +0100

    create user view bids page

--- frontend/src/components/pages/user/Bids.jsx
+++ frontend/src/components/pages/user/Bids.jsx
@@ -8,9 +8,10 @@ import { getTokenFromStore } from 'utils/localStorage';
 import { remainingDays } from 'utils/date-helpers';
 import { eventIsVoid, eventHasExpired } from 'utils/event-helpers';
 import AlertMessage from 'components/common/utils/AlertMessage';
-import ViewEvent from './ViewEvent';
 import Image from 'components/common/utils/Image';
 import DuvLiveModal from 'components/custom/Modal';
+import { moneyFormat, twoDigitNumber } from 'utils/helpers';
+import Stars from 'components/common/utils/Stars';
 
 const Bids = ({ eventEntertainerId }) => {
   const [eventEntertainer, setEventEntertainer] = React.useState({});
@@ -35,8 +36,6 @@ const Bids = ({ eventEntertainerId }) => {
           navigate('/user/auctions');
         });
   }, [eventEntertainerId]);
-
-  console.log('eventEntertainer', eventEntertainer);
 
   if (!eventEntertainer.event) {
     console.log('eventEntertainer.eventDate', eventEntertainer.eventDate);
@@ -65,7 +64,7 @@ const Bids = ({ eventEntertainerId }) => {
 
           {/* Event Details and Entertainers */}
           <aside className="row">
-            <div className="col-md-8">
+            <div className="col-md-12">
               {/* TODO: DAY AUCTION WILL EXPIRE, NO MORE BIDS, TIPS ON SELECTING YOUR BEST PRICE  */}
               {eventHasExpired(eventEntertainer.event.eventDate) && (
                 <AlertMessage message="Event Date has passed" type="error" />
@@ -78,18 +77,18 @@ const Bids = ({ eventEntertainerId }) => {
                   />
                 )}
 
-              <Bids.ApplicationsTable
+              <BidsApplicationsTable
                 applications={eventEntertainer.applications || []}
               />
             </div>
-            <div className="col-md-4">
+            {/* <div className="col-md-4">
               <ViewEvent.EventDetailsCard event={eventEntertainer.event} />
               {!eventHasExpired(eventEntertainer.event.eventDate) && (
                 <div className="text-right cancel-event__text mt-3 mb-5">
                   <i className="icon icon-cancel"></i> Cancel Event
                 </div>
               )}
-            </div>
+            </div> */}
           </aside>
         </section>
       </div>
@@ -105,21 +104,67 @@ Bids.defaultProps = {
   eventEntertainerId: null
 };
 
-Bids.ApplicationsTable = ({ applications }) => {
-  return applications.length > 0 ? (
+const BidsApplicationsTable = ({ applications }) => {
+  const [allApplications, setAllApplications] = React.useState(applications);
+  const [currentSort, setSort] = React.useState('oldest');
+  const sortBids = type => {
+    const sorted = [].concat(allApplications).sort((a, b) => {
+      switch (type) {
+        case 'oldest':
+          return a.id - b.id;
+        case 'newest':
+          return b.id - a.id;
+        case 'lowest':
+          return parseInt(a.askingPrice, 10) - parseInt(b.askingPrice, 10);
+        case 'highest':
+          return parseInt(b.askingPrice, 10) - parseInt(a.askingPrice, 10);
+
+        default:
+          break;
+      }
+      return null;
+    });
+    console.log('sorted', sorted);
+    setAllApplications(sorted);
+    setSort(type);
+  };
+
+  const sortButton = (name, sort) => (
+    <button
+      className={`nav-link btn ${currentSort === sort && 'active'}`}
+      onClick={() => sortBids(sort)}
+    >
+      {name}
+    </button>
+  );
+
+  return allApplications.length > 0 ? (
     <div className="table-responsive">
       <table className="table table-dark  table__no-border table__with-bg">
         <thead>
           <tr className="transparent">
-            <td colSpan="5">
-              <h3 className="event-title text-blue">Current Applications</h3>
+            <td colSpan="7">
+              <h3 className="event-title text-blue">Current Bids</h3>
+              {/* SORT */}
+              <nav className="nav nav-pills nav-sort nav-pull-right">
+                <div className="nav-link disabled btn nav-text">Sort By: </div>
+                {sortButton('Oldest', 'oldest')}
+                {sortButton('Newest', 'newest')}
+                {sortButton('Lowest Price', 'lowest')}
+                {sortButton('Highest Price', 'highest')}
+                {sortButton('Ratings', 'ratings')}
+              </nav>
             </td>
           </tr>
         </thead>
 
         <tbody>
-          {applications.map((application, index) => (
-            <Bids.ApplicationsTableRow application={application} key={index} />
+          {allApplications.map((application, index) => (
+            <Bids.ApplicationsTableRow
+              application={application}
+              key={index}
+              number={index + 1}
+            />
           ))}
         </tbody>
       </table>
@@ -131,11 +176,11 @@ Bids.ApplicationsTable = ({ applications }) => {
   );
 };
 
-Bids.ApplicationsTable.propTypes = {
+BidsApplicationsTable.propTypes = {
   applications: PropTypes.array.isRequired
 };
 
-Bids.ApplicationsTableRow = ({ application }) => {
+Bids.ApplicationsTableRow = ({ application, number }) => {
   if (!application && !application.user && !application.user.profile) {
     return null;
   }
@@ -148,23 +193,38 @@ Bids.ApplicationsTableRow = ({ application }) => {
 
   return (
     <tr>
+      <th className="table__number" scope="row">
+        {twoDigitNumber(number)}
+      </th>
       <td className="align-middle">
         {application && (
           <Image
-            className="avatar--medium"
+            className="avatar--medium-small"
             name={application.user.profile.stageName || 'No name'}
             responsiveImage={false}
             src={application.user.profileImageURL || 'No src'}
           />
         )}
       </td>
-      <td className="align-middle text-right">
+      <td className="align-middle text-gray">
+        <span className="text-muted small--4">Stage name</span>{' '}
         {application.user.profile.stageName}
       </td>
-      <td className="align-middle text-yellow">{application.askingPrice}</td>
+      <td className="align-middle text-yellow">
+        <span className="text-muted small--4">Asking Price</span> &#8358;{' '}
+        {moneyFormat(application.askingPrice)}
+      </td>
+      <td className="align-middle text-white td-btn">
+        <span className="text-muted small--4">Ratings</span>{' '}
+        <Stars name={application.user.profile.stageName} rating={4.5} />
+      </td>
+      <td className="align-middle text-gray">
+        <span className="text-muted small--4">Location</span>{' '}
+        {application.user.profile.location}
+      </td>
       <td className="align-middle text-right td-btn">
         <button className="btn btn-danger btn-sm btn-transparent">
-          Approve Bid
+          Approve
         </button>
       </td>
       <td className="align-middle text-right td-btn">
@@ -176,7 +236,8 @@ Bids.ApplicationsTableRow = ({ application }) => {
   );
 };
 Bids.ApplicationsTableRow.propTypes = {
-  application: PropTypes.object
+  application: PropTypes.object,
+  number: PropTypes.number.isRequired
 };
 Bids.ApplicationsTableRow.defaultProps = {
   application: {}
--- frontend/src/utils/date-helpers.js
+++ frontend/src/utils/date-helpers.js
@@ -14,7 +14,8 @@ import {
  * @param {*} date
  */
 export const getEventDate = date => format(parse(date), 'MMM. D (ddd)');
-export const getShortDate = date => format(parse(date), 'ddd, MMM D YYYY');
+export const getShortDate = date => format(parse(date), 'ddd, MMM D, YYYY');
+export const getTinyDate = date => format(parse(date), 'MMM D, YYYY');
 export const getLongDate = date => format(parse(date), 'dddd, Do MMMM YYYY');
 export const getTime = date => format(parse(date), 'h:mm A');
 export const remainingDays = date => <TimeAgo date={date} />;
