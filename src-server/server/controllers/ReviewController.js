619ddf9757195ec5dcbd68a6d6126720aa814250
Oluwaseyi Aromokeye <aromokeyes2@gmail.com>
2020-04-19 15:51:05 +0100

    add entertainer specific review routes

--- src-server/server/controllers/ReviewController.js
+++ src-server/server/controllers/ReviewController.js
@@ -173,7 +173,111 @@ const ReviewController = {
       });
   },
 
-  // getEntertainerReviews
+  /**
+   * get one Review details
+   * @function
+   * @param {object} req is req object
+   * @param {object} res is res object
+   * @return {object} returns res object
+   */
+  getEntertainerReviews(req, res) {
+    const userId = req.user.id;
+    EntertainerProfile.findOne({
+      where: {
+        userId,
+      },
+    })
+      .then((entertainer) => {
+        Review.findAll({
+          where: {
+            entertainerId: entertainer.id,
+          },
+          include: [
+            {
+              model: Rating,
+              as: 'rating',
+            },
+            {
+              model: Event,
+              as: 'reviewedEvent',
+            },
+            {
+              model: User,
+              as: 'reviewer',
+              attributes: ['id', 'firstName', 'lastName', 'profileImageURL'],
+            },
+          ],
+        })
+          .then((reviews) => {
+            if (!reviews) {
+              return res.status(404).json({ message: 'Review not found' });
+            }
+            return res.status(200).json({ reviews });
+          })
+          .catch((error) => {
+            const errorMessage = error.message || error;
+            return res.status(412).json({ message: errorMessage });
+          });
+      })
+      .catch((error) => {
+        const errorMessage = error.message || error;
+        return res.status(412).json({ message: errorMessage });
+      });
+  },
+
+  /**
+   * get One Entertainer Review
+   * @function
+   * @param {object} req is req object
+   * @param {object} res is res object
+   * @return {object} returns res object
+   */
+  getOneEntertainerReview(req, res) {
+    const userId = req.user.id;
+    const id = req.params.id;
+    EntertainerProfile.findOne({
+      where: {
+        userId,
+      },
+    })
+      .then((entertainer) => {
+        Review.findOne({
+          where: {
+            id,
+            entertainerId: entertainer.id,
+          },
+          include: [
+            {
+              model: Rating,
+              as: 'rating',
+            },
+            {
+              model: Event,
+              as: 'reviewedEvent',
+            },
+            {
+              model: User,
+              as: 'reviewer',
+              attributes: ['id', 'firstName', 'lastName', 'profileImageURL'],
+            },
+          ],
+        })
+          .then((reviews) => {
+            if (!reviews) {
+              return res.status(404).json({ message: 'Review not found' });
+            }
+            return res.status(200).json({ reviews });
+          })
+          .catch((error) => {
+            const errorMessage = error.message || error;
+            return res.status(412).json({ message: errorMessage });
+          });
+      })
+      .catch((error) => {
+        const errorMessage = error.message || error;
+        return res.status(412).json({ message: errorMessage });
+      });
+  },
 };
 
 export default ReviewController;
