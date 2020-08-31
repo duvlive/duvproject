5be33e33dd7b63c56e1e885a4fd3a2ca8ab4420d
Oluwaseyi Aromokeye <aromokeyes2@gmail.com>
2020-08-31 13:47:20 +0100

    refactor

--- src-server/server/MailSender.js
+++ src-server/server/MailSender.js
@@ -59,29 +59,29 @@ export default async function sendMail(content, user, additionalOptions = {}) {
         }
       }
     };
+  } else {
+    // using smtp
+    let transporter = nodemailer.createTransport({
+      host: process.env.EMAIL_SERVER || 'smtp.mailtrap.io',
+      port: process.env.EMAIL_PORT || 2525,
+      secure: !!process.env.EMAIL_SECURE || false,
+      auth: {
+        user: process.env.EMAIL_USER || process.env.MAIL_TRAP_USER,
+        pass: process.env.EMAIL_PASS || process.env.MAIL_TRAP_PASS,
+      },
+    });
+    // ensure userEmail is always present
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail(message);
+
+    console.log('Message sent: %s', info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+    // Preview only available when sending through an Ethereal account
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+
+    // html: ejs.render( fs.readFileSync('e-mail.ejs', 'utf-8') , {mensagem: 'olá, funciona'})
   }
-
-  // using smtp
-  let transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_SERVER || 'smtp.mailtrap.io',
-    port: process.env.EMAIL_PORT || 2525,
-    secure: !!process.env.EMAIL_SECURE || false,
-    auth: {
-      user: process.env.EMAIL_USER || process.env.MAIL_TRAP_USER,
-      pass: process.env.EMAIL_PASS || process.env.MAIL_TRAP_PASS,
-    },
-  });
-  // ensure userEmail is always present
-
-  // send mail with defined transport object
-  let info = await transporter.sendMail(message);
-
-  console.log('Message sent: %s', info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-  // Preview only available when sending through an Ethereal account
-  console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-
-  // html: ejs.render( fs.readFileSync('e-mail.ejs', 'utf-8') , {mensagem: 'olá, funciona'})
 }
