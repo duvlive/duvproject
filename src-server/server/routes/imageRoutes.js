065557d8616603630a3932a9597eb6eee349a68b
Haruna Popoola <haruna.popoola@yahoo.com>
2020-09-20 08:35:49 +0100

    create image endpoint

--- src-server/server/routes/imageRoutes.js
+++ src-server/server/routes/imageRoutes.js
@@ -1,13 +1,21 @@
 import { ImageController } from '../controllers';
 import Authentication from '../middleware/authentication';
 
-const imageRoutes = router => {
+const imageRoutes = (router) => {
   router.post(
     '/api/v1/upload-profile-image',
     Authentication.verifyToken,
     Authentication.isActiveUser,
     ImageController.uploadImage,
     ImageController.saveImage
+  );
+
+  router.post(
+    '/api/v1/upload-article-image',
+    Authentication.verifyToken,
+    Authentication.isActiveUser,
+    ImageController.uploadArticleImage,
+    ImageController.getUploadedImage
   );
 };
 
