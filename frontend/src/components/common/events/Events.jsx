7a18a5c5371dc61d88fc221249f74caf6439d685
Haruna Popoola <haruna.popoola@yahoo.com>
2020-10-11 13:53:32 +0100

    update event status to pending after each update

--- frontend/src/components/common/events/Events.jsx
+++ frontend/src/components/common/events/Events.jsx
@@ -2,15 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from '@reach/router';
 import { Col, Card, CardImg, CardImgOverlay } from 'reactstrap';
-import { format, parse } from 'date-fns';
 import defaultImage from 'assets/img/events/public-event.jpg';
-import { getTime } from 'utils/date-helpers';
+import { getPublicEventDate } from 'utils/date-helpers';
 
-const Events = ({ mainImage, location, slug, startTime, title }) => {
-  const eventDate = parse(startTime);
-  const weekDay = format(eventDate, 'ddd');
-  const fullDate = format(eventDate, 'MMM D');
-
+const Events = ({ mainImage, location, slug, startTime, endTime, title }) => {
   return (
     <Col sm={4}>
       <Link to={`/event/${slug}`}>
@@ -24,14 +19,12 @@ const Events = ({ mainImage, location, slug, startTime, title }) => {
             />
             <CardImgOverlay />
           </div>
-          <div className="event-card__body">
-            <div className="event-card__datetime">
-              <span className="event-card__weekday">{weekDay},</span>
-              <span className="event-card__date"> {fullDate},</span>
-              <span className="event-card__time">{getTime(startTime)}</span>
-            </div>
+          <div className="event-card__body text-left">
             <div className="event-card__info">
               <h6 className="event-card__title text-truncate">{title}</h6>
+              <span className="event-card__date">
+                {getPublicEventDate(startTime, endTime)}
+              </span>
               <p className="event-card__address">{location}</p>
             </div>
           </div>
@@ -42,6 +35,7 @@ const Events = ({ mainImage, location, slug, startTime, title }) => {
 };
 
 Events.propTypes = {
+  endTime: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
   mainImage: PropTypes.string,
   slug: PropTypes.string.isRequired,
