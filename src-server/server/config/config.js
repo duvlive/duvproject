b252411c5d126e9047a57def5c6d31e9c4cb5f30
Ufuoma Gideon Iboyi <u.iboyi@heunets.com>
2025-09-30 11:57:50 +0100

    ready to deploy

---
+++ src-server/server/config/config.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+console.log( process.env.DATABASE_URL)
+
+module.exports = {
+  development: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres'
+  },
+  test: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres'
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres'
+  }
+};
