b4bda9b54e336efcfc0c5a787770e1e0550403ec
Ufuoma Gideon Iboyi <u.iboyi@heunets.com>
2025-10-08 10:01:05 +0100

    script updated for mobile number

--- src-server/scripts/createAdmin.js
+++ src-server/scripts/createAdmin.js
@@ -20,6 +20,7 @@ async function main() {
       lastName,
       email,
       password,
+      phoneNumber: '08100000003',
       type: USER_TYPES.ADMINISTRATOR,
       isActive: true,
       activatedAt: new Date(),
