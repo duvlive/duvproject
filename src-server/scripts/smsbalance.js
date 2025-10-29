b7ad205965075e9675c92703736f791ac9311301
Ufuoma Gideon Iboyi <u.iboyi@heunets.com>
2025-10-29 03:00:54 +0100

    script run

--- src-server/scripts/smsbalance.js
+++ src-server/scripts/smsbalance.js
@@ -3,20 +3,20 @@ const db = require('../server/models');
 (async () => {
   try {
     
-    await db.sequelize.query('DROP TABLE IF EXISTS "SmsBalances" CASCADE;');
-    console.log('Dropped old SmsBalances table from Railway DB');
+    await db.sequelize.query('DROP TYPE IF EXISTS "SmsBalances" CASCADE;');
+    console.log(' Dropped lingering Postgres type "SmsBalances"');
 
     
-    await db.sequelize.authenticate();
-    console.log('Database connection verified');
+    await db.sequelize.query('DROP TABLE IF EXISTS "SmsBalances" CASCADE;');
+    console.log('Dropped table "SmsBalances"');
 
     
     await db.SmsBalance.sync({ force: true });
-    console.log('SmsBalance table recreated successfully');
+    console.log(' SmsBalance table recreated successfully');
 
     process.exit(0);
   } catch (err) {
-    console.error(' Failed to drop SmsBalances table:', err);
+    console.error(' Failed to fix SmsBalances:', err);
     process.exit(1);
   }
 })();
